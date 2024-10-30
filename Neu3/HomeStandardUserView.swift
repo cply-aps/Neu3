e1208f68d5e13dc6e0da6bc0cf3ae299456bf44b
Casper L <cl@cply.dk>
2024-10-30 20:56:57 +0100

    LoginView only missing direction depending on userrole

--- Neu3/HomeStandardUserView.swift
+++ Neu3/HomeStandardUserView.swift
@@ -11,26 +11,45 @@ struct HomeView: View, Hashable {
     var username: String
     
     var body: some View {
-        VStack {
-            Text("Welcome \(username) to the Home View!")
-                .font(.largeTitle)
-                .padding()
-            
-            Button(action: {
-                // Log out by resetting the root view to LoginView
-                if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
-                   let window = windowScene.windows.first {
-                    window.rootViewController = UIHostingController(rootView: LoginView())
-                    window.makeKeyAndVisible()
-                }
-            }) {
-                Text("Logout")
-                    .foregroundColor(.white)
+        NavigationStack{
+            VStack {
+                Image("CP_Trold_Vaegt")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                Text("Welcome \(username) to the Home View!")
+                    .font(.largeTitle)
                     .padding()
-                    .background(Color.red)
-                    .cornerRadius(8)
+                Button(action: {
+                    // Log out by resetting the root view to LoginView
+                    if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                       let window = windowScene.windows.first {
+                        window.rootViewController = UIHostingController(rootView: LoginView())
+                        window.makeKeyAndVisible()
+                    }
+                }) {
+                    Text("Logout")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.red)
+                        .cornerRadius(8)
+                }
+                .padding()
             }
-            .padding()
+            .navigationTitle("Dagbog")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) { // Place the button on the left side
+                    Button {
+                        //TODO: Create ProfileView and link to it here
+                    }label: {
+                        Image(systemName: "person.fill")
+                            .imageScale(.large)
+                        Text("profil")
+                            .textScale(.default)
+                        
+                    }
+                }
+            }
         }
     }
 }
--- Neu3/LoginView.swift
+++ Neu3/LoginView.swift
@@ -18,42 +18,66 @@ struct LoginView: View {
     }
     
     var body: some View {
-        VStack(spacing: 20) {
-            // Username TextField
-            TextField("Username", text: $viewModel.username)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-                .focused($focusedField, equals: .username)
-                .submitLabel(.next)
-                .onSubmit {
-                    focusedField = .password
+            NavigationStack{
+                ZStack{
+                    Color.blue.opacity(0.2) // Adjust the color and opacity as needed
+                            .edgesIgnoringSafeArea(.all) // Make sure the color fills the entire screen
+                    
+                    ScrollView {
+                        VStack(spacing: 20) {
+                            Image("CP_Trold_Vaegt")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 250, height: 250)
+                            // Username TextField
+                            TextField("Username", text: $viewModel.username)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .padding()
+                                .focused($focusedField, equals: .username)
+                                .submitLabel(.next)
+                                .onSubmit {
+                                    focusedField = .password
+                                }
+                            
+                            // Password SecureField
+                            SecureField("Password", text: $viewModel.password)
+                                .textFieldStyle(RoundedBorderTextFieldStyle())
+                                .padding()
+                                .focused($focusedField, equals: .password)
+                                .submitLabel(.go)
+                                .onSubmit {
+                                    viewModel.login()
+                                }
+                            
+                            // Forgotten password button
+                            Button("Forgotten login credentials?") {
+                                //TODO: Create "forgottenPassword" POST Method
+                            }
+                            Spacer()
+                            
+                            // Error message display
+                            if !viewModel.errorMessage.isEmpty {
+                                Text(viewModel.errorMessage)
+                                    .foregroundColor(.red)
+                                    .padding()
+                            }
+                        }
+                        .padding()
+                        .navigationTitle("Login")
+                        .navigationBarTitleDisplayMode(.inline)
+                        .onTapGesture {
+                            // Dismiss the keyboard when tapping outside the fields
+                            focusedField = nil
+                        }
+                    }
+                .onAppear {
+                    focusedField = .username
                 }
-            
-            // Password SecureField
-            SecureField("Password", text: $viewModel.password)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding()
-                .focused($focusedField, equals: .password)
-                .submitLabel(.go)
-                .onSubmit {
-                    viewModel.login()
+                // Watch for isLoggedIn to change the root view
+                .fullScreenCover(isPresented: $viewModel.isLoggedIn) {
+                    HomeView(username: viewModel.username)
                 }
-            
-            // Error message display
-            if !viewModel.errorMessage.isEmpty {
-                Text(viewModel.errorMessage)
-                    .foregroundColor(.red)
-                    .padding()
             }
-        }
-        .padding()
-        .navigationTitle("Login")
-        .onAppear {
-            focusedField = .username
-        }
-        // Watch for isLoggedIn to change the root view
-        .fullScreenCover(isPresented: $viewModel.isLoggedIn) {
-            HomeView(username: viewModel.username)
         }
     }
 }
