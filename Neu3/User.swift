15dedeab1743fcf3c6e7798636f12dfc5cc7c0a0
Casper L <cl@cply.dk>
2024-11-02 22:10:33 +0100

    Deleted a lot of properties from the User Model

--- Neu3/User.swift
+++ Neu3/User.swift
@@ -9,37 +9,11 @@
 import Foundation
 
 struct User: Codable {
-    let id: String
     let userName: String
-    let normalizedUserName: String
-    let email: String
-    let normalizedEmail: String
-    let emailConfirmed: Bool
     let passwordHash: String
-    let securityStamp: String
-    let concurrencyStamp: String
-    let phoneNumber: String?
-    let phoneNumberConfirmed: Bool
-    let twoFactorEnabled: Bool
-    let lockoutEnd: String? // Or Date, depending on how it's returned
-    let lockoutEnabled: Bool
-    let accessFailedCount: Int
 }
 
 struct LoginResponse: Codable {
-    let id: String
     let userName: String
-    let normalizedUserName: String
-    let email: String
-    let normalizedEmail: String
-    let emailConfirmed: Bool
     let passwordHash: String
-    let securityStamp: String
-    let concurrencyStamp: String
-    let phoneNumber: String?
-    let phoneNumberConfirmed: Bool
-    let twoFactorEnabled: Bool
-    let lockoutEnd: String?
-    let lockoutEnabled: Bool
-    let accessFailedCount: Int
 }
